5296654b5a47fa373af272137c5f93b391a2bef7
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-05-05 12:54:13 +1200

    Create and install users app
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- commendationSite/settings.py
+++ commendationSite/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 # Application definition
 
 INSTALLED_APPS = [
+	'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
