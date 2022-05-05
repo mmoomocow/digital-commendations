254b5e1a123f466a28ddeb0f4badcfe065fd7331
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-05-05 12:29:05 +1200

    Split allowed hosts in settings.py
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- commendationSite/settings.py
+++ commendationSite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'true') == 'true'
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*')]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
