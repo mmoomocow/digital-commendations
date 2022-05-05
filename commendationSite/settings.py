e051d79bb38c10e09310638317ca7557072ebd85
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-05-05 12:35:34 +1200

    Deafult debug to False and split allowed hosts
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- commendationSite/settings.py
+++ commendationSite/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'true') == 'true'
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
