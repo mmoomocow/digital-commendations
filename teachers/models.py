fecf3f5d97c4a8b51b74299e5eebc9a3ead68db0
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-05-12 12:44:07 +1200

    Teacher model
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- teachers/models.py
+++ teachers/models.py
@@ -3,4 +3,35 @@ from django.db import models
 # Create your models here.
 
 class Teacher(models.Model):
-	pass
+	"""
+		The model for teachers and staff.
+
+		This model will link to commendations, and contain data that is 
+		specific to staff members only, and therefore not suitable for the
+		generic user model.
+
+		The generic user model contains all of the basic information that
+		is required for a user to log in, and contains email address, name
+		password etc, so this information should not be stored in the teacher model
+	"""
+
+	# Teacher ID
+	# Used internally to uniquely identify teachers
+	id = models.AutoField(primary_key=True, unique=True, editable=False, blank=False, null=False, verbose_name='ID')
+
+	# Staff code
+	# Uniquely identifies the teacher but can be repeated once a teacher has left the school.
+	staff_code = models.CharField(max_length=2, unique=True, blank=False, null=False, verbose_name='Staff Code')
+
+	# House group
+	house_group = models.Choices((('Anderson', 'Anderson'), ('Begg', 'Begg'), ('Ross', 'Ross'), ('Herron', 'Herron'), ('Somerville', 'Somerville')))
+
+	class Meta:
+		ordering = ('staff_code',)
+
+	def __str__(self):
+		return self.staff_code
+
+	def delete(self, *args, **kwargs):
+		# Delete the teacher
+		super().delete(*args, **kwargs)
