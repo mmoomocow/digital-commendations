3bc597b3bf20c88f488e695922fc3c36394e846d
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-06-01 10:43:41 +1200

    Add basic data to user admin model list display, filter options, search fields, default ordering and readonly fields
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,8 @@ from .models import *
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-	pass
+	list_display = ('username', 'first_name', 'last_name', 'email', 'is_teacher', 'is_student', 'is_caregiver')
+	list_filter = ('is_teacher', 'is_student', 'is_caregiver', 'is_staff', 'is_superuser', 'is_active')
+	search_fields = ('username', 'email', 'first_name', 'last_name')
+	ordering = ('first_name', 'last_name')
+	readonly_fields = ('last_login', 'id')
