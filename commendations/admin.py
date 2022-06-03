73fc4f410680725484884103faed3275b43909d2
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-06-03 20:57:32 +1200

    Add commendations to the admin backend
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- commendations/admin.py
+++ commendations/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(commendation)
+class CommendationAdmin(admin.ModelAdmin):
+	list_display = ('id', 'commendation_type', 'date_time', 'teacher')
+	list_filter = ('commendation_type', 'date_time', 'teacher')
+	search_fields = ('id', 'commendation_type', 'date_time', 'teacher', 'reason', 'students')
+	readonly_fields = ('id', 'date_time')
+	filter_horizontal = ('students',)
+	fieldsets = (
+		(None, {
+			'fields': ('id',)
+		}),
+		('Commendation', {
+			'fields': ('commendation_type', 'reason', 'date_time')
+		}),
+		('User Links', {
+			'fields': ('teacher', 'students')
+		}),
+	)
