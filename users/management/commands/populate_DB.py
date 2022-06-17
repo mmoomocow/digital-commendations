b92ff36481c1e2e33ddb0272ed08c5bc4c0a6221
mmoomocow <matthew@mmoomocow.tech>
2022-06-17 20:39:33 +1200

    Dummy data generator Uses Faker to generate users for the database and links to student and teachers Use python manage.py <number of teachers> <number of students>
    Signed-off-by: mmoomocow <matthew@mmoomocow.tech>

---
+++ users/management/commands/populate_DB.py
@@ -0,0 +1,75 @@
+from random import choice
+from django.core.management.base import BaseCommand, CommandError
+from faker import Faker
+from faker.providers import profile as fake_profile
+from faker.providers import misc as fake_misc
+from users import models as user_models
+from students import models as student_models
+from teachers import models as teacher_models
+from commendations import models as commendation_models
+
+fake = Faker()
+fake.add_provider(fake_profile)
+fake.add_provider(fake_misc)
+
+def generate_teacher(number):
+	teacher_houses = [teacher_models.Teacher.ANDERSON, teacher_models.Teacher.BEGG, teacher_models.Teacher.HERRON, teacher_models.Teacher.ROSS, teacher_models.Teacher.SOMERVILLE]
+	for _ in range(number):
+		# Generate a profile for the dummy teacher
+		profile = fake.simple_profile()
+		# Create the dummy user object
+		user = user_models.User.objects.create_user(
+			username=profile['username'],
+			password=fake.password(length=12),
+			first_name=profile['name'].split(' ')[0],
+			last_name=profile['name'].split(' ')[1],
+			email=profile['mail'],
+		)
+		user.is_teacher = True
+		user.teacher = teacher_models.Teacher.objects.create(
+			staff_code=profile['name'].split(' ')[0:2],
+			house_group=choice(teacher_houses),
+		)
+		user.save()
+
+def generate_student(number):
+	student_houses = [student_models.Student.ANDERSON, student_models.Student.BEGG, student_models.Student.HERRON, student_models.Student.ROSS, student_models.Student.SOMERVILLE]
+	for _ in range(number):
+		# Generate a profile for the dummy student
+		profile = fake.simple_profile()
+		# Create the dummy user object
+		user = user_models.User.objects.create_user(
+			username=profile['username'],
+			password=fake.password(length=12),
+			first_name=profile['name'].split(' ')[0],
+			last_name=profile['name'].split(' ')[1],
+			email=profile['mail'],
+		)
+		user.is_student = True
+		user.student = student_models.Student.objects.create(
+			id=fake.unique.numerify(text='#####'),
+			tutor_room=fake.lexify(text='???'),
+			house_group=choice(student_houses),
+			year_level=fake.random_int(min=9, max=13),
+		)
+		user.save()
+
+class Command(BaseCommand):
+	help = 'Populates the database with dummy data.'
+
+	def add_arguments(self, parser):
+		parser.add_argument('teachers', nargs='+', type=int)
+		parser.add_argument('students', nargs='+', type=int)
+
+	def handle(self, *args, **options):
+		self.stdout.write(self.style.NOTICE('Populating the database...'))
+
+		self.stdout.write(self.style.NOTICE('Creating %s teachers...' % options['teachers'][0]))
+		generate_teacher(options['teachers'][0])
+		self.stdout.write(self.style.SUCCESS('Done!\n\n'))
+
+		self.stdout.write(self.style.NOTICE('Creating %s students...' % options['students'][0]))
+		generate_student(options['students'][0])
+		self.stdout.write(self.style.SUCCESS('Done!\n\n'))
+
+		self.stdout.write(self.style.SUCCESS('Successfully generated %s users' % (options['teachers'][0] + options['students'][0])))
