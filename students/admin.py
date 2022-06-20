eadee58de9af5a380c382702ca00bb5e1dbe432a
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-06-21 09:34:16 +1200

    Add searchability to students from admin page
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- students/admin.py
+++ students/admin.py
@@ -7,6 +7,7 @@ from .models import *
 class StudentAdmin(admin.ModelAdmin):
 	list_display = ('id', 'user', 'tutor_room', 'house_group', 'year_level',)
 	list_filter = ('house_group', 'year_level',)
+	search_fields = ('id', 'user__first_name', 'user__last_name')
 
 @admin.register(Caregiver)
 class CaregiverAdmin(admin.ModelAdmin):
