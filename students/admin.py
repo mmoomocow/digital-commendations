059e0e28e87c370be3af0152614d2bea73b0cb93
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-06-21 09:35:16 +1200

    Order students by ID on admin page
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- students/admin.py
+++ students/admin.py
@@ -8,6 +8,7 @@ class StudentAdmin(admin.ModelAdmin):
 	list_display = ('id', 'user', 'tutor_room', 'house_group', 'year_level',)
 	list_filter = ('house_group', 'year_level',)
 	search_fields = ('id', 'user__first_name', 'user__last_name')
+	ordering = ('id', 'user__first_name', 'user__last_name')
 
 @admin.register(Caregiver)
 class CaregiverAdmin(admin.ModelAdmin):
