89e0207fbe94e5e6c03398f3f4482e04d036de89
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-06-23 22:19:02 +1200

    Basic login view Doesn't return login page with form, as none exist
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+def loginView(request):
+	if request.method == 'POST':
+		# Triggered if the client has filled out and submitted the form
+		username = request.POST['username']
+		password = request.POST['password']
+		# Checks if the user is valid
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			# Restrict access to active teachers for now
+			if user.is_active:
+				login(request, user)
+				return HttpResponse('<h1>Login successful</h1>')
+
+	else:
+		# Triggered if the client has not filled out the form, so send them the login page
+		return HttpResponse('<h1>Login</h1>') # Basic HTTP response for now without a form, but we'll add one later
