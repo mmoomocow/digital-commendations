35ad424ffd32826203a6d92d3b36ca8b744e60da
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-06-24 10:33:19 +1200

    Add logout view
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -20,3 +20,7 @@ def loginView(request):
 	else:
 		# Triggered if the client has not filled out the form, so send them the login page
 		return HttpResponse('<h1>Login</h1>') # Basic HTTP response for now without a form, but we'll add one later
+
+def logoutView(request):
+	logout(request)
+	return HttpResponse('<h1>Logged Out</h1>') # Basic HTTP more work on front end will be done later
