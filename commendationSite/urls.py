b459e4b5d10325ce4b2dacba15b7c065e50276de
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-06-24 11:19:10 +1200

    redirect /login/ to /users/login/
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- commendationSite/urls.py
+++ commendationSite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('api/', include('api.urls')),
+	path('login/', RedirectView.as_view(url='/users/login/')),
+	path('logout/', RedirectView.as_view(url='/users/logout/')),
 	path('users/', include('users.urls')),
 ]
