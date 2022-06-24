a83d66e0d6ce659f143b46559c877a1e6edd9351
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-06-24 17:35:56 +1200

    Unit tests for login view
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- users/tests.py
+++ users/tests.py
@@ -20,6 +20,15 @@ class UserTestCase(TestCase):
 			first_name='Test',
 			last_name='SuperUser',
 		)
+		self.webLogin = User.objects.create_user(
+			username='testWebLogin',
+			email='testWebLogin@example.com',
+			password='testpassword',
+			first_name='Test',
+			last_name='WebLogin',
+		)
+		self.webLogin.is_teacher = True
+		self.webLogin.save()
 
 	def test_user_creation(self):
 		self.assertEqual(self.user.username, 'testuser', 'Username is not correct')
@@ -39,7 +48,7 @@ class UserTestCase(TestCase):
 		self.assertEqual(self.superuser.last_name, 'SuperUser', 'Last name is not correct')
 		self.assertEqual(str(self.superuser), 'Test SuperUser', 'User string representation is not correct')
 
-	def test_user_permsissions(self):
+	def test_user_permissions(self):
 		self.assertFalse(self.user.is_staff, 'User is staff')
 		self.assertFalse(self.user.is_superuser, 'User is superuser')
 		self.assertTrue(self.user.is_active, 'User is not active')
@@ -48,3 +57,11 @@ class UserTestCase(TestCase):
 		self.assertTrue(self.superuser.is_staff, 'Superuser is not staff')
 		self.assertTrue(self.superuser.is_superuser, 'Superuser is not superuser')
 		self.assertTrue(self.superuser.is_active, 'Superuser is not active')
+
+	def test_user_login(self):
+		response1 = self.client.post('/users/login/', data={'username': 'testWebLogin', 'password': 'testpassword'})
+		self.assertEqual(response1.status_code, 202, 'Correct username and password was not accepted')
+		response2 = self.client.post('/users/login/', data={'username': 'testWebLogin', 'password': 'testpassword2'})
+		self.assertEqual(response2.status_code, 403, 'Incorrect username and password was accepted')
+		response3 = self.client.post('/users/login/', data={'username': 'testuser', 'password': 'testpassword'})
+		self.assertEqual(response3.status_code, 403, 'Non teacher user was logged in')
