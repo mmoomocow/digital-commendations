7889283e4786ab0c20732039ca141908caa6b99e
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-06-25 17:20:29 +1200

    Remove unused imports
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import loginView, logoutView
 
 urlpatterns = [
     path("login/", loginView, name="login"),
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 
