89e9e80a35182c5018f51f3e19b306d66afa436f
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-06-25 15:36:47 +1200

    Format all files with Black Use git ignore-rev to stop this showing in git blame
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- commendations/tests.py
+++ commendations/tests.py
@@ -6,75 +6,95 @@ from teachers import models as teacher_models
 
 # Create your tests here.
 
+
 class CommendationTestCase(TestCase):
-	def setUp(self):
-		# Commendation needs teachers and students
-		# First create a teacher
-		self.teacher = user=user_models.User.objects.create_user(
-				username='CommendationTeacher',
-				email='testTeacher@example.com',
-				password='teacherpassword',
-				first_name='Commendation',
-				last_name='Teacher',
-			)
-		self.teacher.is_teacher = True
-		self.teacher.teacher = teacher_models.Teacher.objects.create(
-			staff_code='Ab',
-			house_group=teacher_models.Teacher.ANDERSON,
-		)
-		self.teacher.save()
+    def setUp(self):
+        # Commendation needs teachers and students
+        # First create a teacher
+        self.teacher = user = user_models.User.objects.create_user(
+            username="CommendationTeacher",
+            email="testTeacher@example.com",
+            password="teacherpassword",
+            first_name="Commendation",
+            last_name="Teacher",
+        )
+        self.teacher.is_teacher = True
+        self.teacher.teacher = teacher_models.Teacher.objects.create(
+            staff_code="Ab",
+            house_group=teacher_models.Teacher.ANDERSON,
+        )
+        self.teacher.save()
 
-		# Then create 2 students
-		self.student1 = user_models.User.objects.create_user(
-			username='CommendationStudent1',
-			email='commendationStudent1@example.com',
-			password='studentpassword',
-			first_name='Commendation',
-			last_name='Student1',
-		)
-		self.student1.is_student = True
-		self.student1.student=student_models.Student.objects.create(
-			id='23456',
-			tutor_room='abc',
-			house_group=student_models.Student.BEGG,
-			year_level=student_models.Student.YEAR9,
-		)
-		self.student1.save()
+        # Then create 2 students
+        self.student1 = user_models.User.objects.create_user(
+            username="CommendationStudent1",
+            email="commendationStudent1@example.com",
+            password="studentpassword",
+            first_name="Commendation",
+            last_name="Student1",
+        )
+        self.student1.is_student = True
+        self.student1.student = student_models.Student.objects.create(
+            id="23456",
+            tutor_room="abc",
+            house_group=student_models.Student.BEGG,
+            year_level=student_models.Student.YEAR9,
+        )
+        self.student1.save()
 
-		self.student2 = user_models.User.objects.create_user(
-			username='CommendationStudent2',
-			email='commendationStudent2@example.com',
-			password='studentpassword',
-			first_name='Commendation',
-			last_name='Student2',
-		)
-		self.student2.is_student = True
-		self.student2.student=student_models.Student.objects.create(
-			id='34567',
-			tutor_room='def',
-			house_group=student_models.Student.BEGG,
-			year_level=student_models.Student.YEAR9,
-		)
-		self.student2.save()
+        self.student2 = user_models.User.objects.create_user(
+            username="CommendationStudent2",
+            email="commendationStudent2@example.com",
+            password="studentpassword",
+            first_name="Commendation",
+            last_name="Student2",
+        )
+        self.student2.is_student = True
+        self.student2.student = student_models.Student.objects.create(
+            id="34567",
+            tutor_room="def",
+            house_group=student_models.Student.BEGG,
+            year_level=student_models.Student.YEAR9,
+        )
+        self.student2.save()
 
-		# Then create a commendation with the teachers and students linked
-		self.commendation = commendation.objects.create(
-			commendation_type=commendation.RESPECT,
-			reason='Cupcake ipsum dolor sit amet sweet roll cheesecake jelly. Soufflé carrot cake sesame snaps toffee pie bears chocolate. Muffin halvah bonbon fruitcake marshmallow sweet roll.',
-			teacher=self.teacher.teacher,
-		)
-		self.commendation.students.add(self.student1.student, self.student2.student)
+        # Then create a commendation with the teachers and students linked
+        self.commendation = commendation.objects.create(
+            commendation_type=commendation.RESPECT,
+            reason="Cupcake ipsum dolor sit amet sweet roll cheesecake jelly. Soufflé carrot cake sesame snaps toffee pie bears chocolate. Muffin halvah bonbon fruitcake marshmallow sweet roll.",
+            teacher=self.teacher.teacher,
+        )
+        self.commendation.students.add(self.student1.student, self.student2.student)
 
-	def test_commendation_creation(self):
-		self.assertEqual(self.commendation.commendation_type, commendation.RESPECT, 'Commendation type is not correct')
-		self.assertEqual(self.commendation.reason, 
-			'Cupcake ipsum dolor sit amet sweet roll cheesecake jelly. Soufflé carrot cake sesame snaps toffee pie bears chocolate. Muffin halvah bonbon fruitcake marshmallow sweet roll.',
-			'Commendation reason is not correct'
-		)
+    def test_commendation_creation(self):
+        self.assertEqual(
+            self.commendation.commendation_type,
+            commendation.RESPECT,
+            "Commendation type is not correct",
+        )
+        self.assertEqual(
+            self.commendation.reason,
+            "Cupcake ipsum dolor sit amet sweet roll cheesecake jelly. Soufflé carrot cake sesame snaps toffee pie bears chocolate. Muffin halvah bonbon fruitcake marshmallow sweet roll.",
+            "Commendation reason is not correct",
+        )
 
-	def test_commendation_links(self):
-		self.assertEqual(self.commendation.teacher, self.teacher.teacher, 'Commendation teacher is not correct')
-		self.assertEqual(self.commendation.students.count(), 2, 'Commendation students are not correct')
-		# The order of students is not important or guaranteed, so we only check if both students are in the list
-		self.assertTrue(self.commendation.students.contains(self.student1.student), 'Commendation student #1 is not correct')
-		self.assertTrue(self.commendation.students.contains(self.student2.student), 'Commendation student #2 is not correct')
+    def test_commendation_links(self):
+        self.assertEqual(
+            self.commendation.teacher,
+            self.teacher.teacher,
+            "Commendation teacher is not correct",
+        )
+        self.assertEqual(
+            self.commendation.students.count(),
+            2,
+            "Commendation students are not correct",
+        )
+        # The order of students is not important or guaranteed, so we only check if both students are in the list
+        self.assertTrue(
+            self.commendation.students.contains(self.student1.student),
+            "Commendation student #1 is not correct",
+        )
+        self.assertTrue(
+            self.commendation.students.contains(self.student2.student),
+            "Commendation student #2 is not correct",
+        )
--- students/models.py
+++ students/models.py
@@ -2,62 +2,86 @@ from django.db import models
 
 # Create your models here.
 
+
 class Caregiver(models.Model):
-	pass
+    pass
+
 
 class Student(models.Model):
-	"""
-		The model for students.
+    """
+    The model for students.
 
-		This model will link to commendations, and contain data that is
-		specific to students only, and therefore not suitable for the
-		generic user model.
-	"""
+    This model will link to commendations, and contain data that is
+    specific to students only, and therefore not suitable for the
+    generic user model.
+    """
 
-	# The KAMAR assigned student ID is a unique identifier for the student
-	id = models.IntegerField(unique=True, primary_key=True, blank=False, null=False, verbose_name='Student ID')
+    # The KAMAR assigned student ID is a unique identifier for the student
+    id = models.IntegerField(
+        unique=True,
+        primary_key=True,
+        blank=False,
+        null=False,
+        verbose_name="Student ID",
+    )
 
-	# The student's tutor room
-	tutor_room = models.CharField(max_length=3, blank=True, null=True, verbose_name='Tutor Room')
-	# The student's house group
-	ANDERSON = 'A'
-	BEGG = 'B'
-	ROSS = 'R'
-	HERRON = 'H'
-	SOMERVILLE = 'S'
-	HOUSE_GROUP_CHOICES = (
-		(ANDERSON, 'Anderson'),
-		(BEGG, 'Begg'),
-		(ROSS, 'Ross'),
-		(HERRON, 'Herron'),
-		(SOMERVILLE, 'Somerville'),
-	)
-	house_group = models.CharField(max_length=20, choices=HOUSE_GROUP_CHOICES, blank=True, null=True, verbose_name='House Group')
-	# The student's year level
-	YEAR9 = 9
-	YEAR10 = 10
-	YEAR11 = 11
-	YEAR12 = 12
-	YEAR13 = 13
-	YEAR_LEVEL_CHOICES = (
-		(YEAR9, 'Year 9'),
-		(YEAR10, 'Year 10'),
-		(YEAR11, 'Year 11'),
-		(YEAR12, 'Year 12'),
-		(YEAR13, 'Year 13'),
-	)
-	year_level = models.IntegerField(choices=YEAR_LEVEL_CHOICES, blank=True, null=True, verbose_name='Year Level')
+    # The student's tutor room
+    tutor_room = models.CharField(
+        max_length=3, blank=True, null=True, verbose_name="Tutor Room"
+    )
+    # The student's house group
+    ANDERSON = "A"
+    BEGG = "B"
+    ROSS = "R"
+    HERRON = "H"
+    SOMERVILLE = "S"
+    HOUSE_GROUP_CHOICES = (
+        (ANDERSON, "Anderson"),
+        (BEGG, "Begg"),
+        (ROSS, "Ross"),
+        (HERRON, "Herron"),
+        (SOMERVILLE, "Somerville"),
+    )
+    house_group = models.CharField(
+        max_length=20,
+        choices=HOUSE_GROUP_CHOICES,
+        blank=True,
+        null=True,
+        verbose_name="House Group",
+    )
+    # The student's year level
+    YEAR9 = 9
+    YEAR10 = 10
+    YEAR11 = 11
+    YEAR12 = 12
+    YEAR13 = 13
+    YEAR_LEVEL_CHOICES = (
+        (YEAR9, "Year 9"),
+        (YEAR10, "Year 10"),
+        (YEAR11, "Year 11"),
+        (YEAR12, "Year 12"),
+        (YEAR13, "Year 13"),
+    )
+    year_level = models.IntegerField(
+        choices=YEAR_LEVEL_CHOICES, blank=True, null=True, verbose_name="Year Level"
+    )
 
-	# Parent/caregiver
-	caregiver = models.ForeignKey(Caregiver, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Caregiver')
+    # Parent/caregiver
+    caregiver = models.ForeignKey(
+        Caregiver,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name="Caregiver",
+    )
 
-	class Meta():
-		verbose_name = 'Student'
-		verbose_name_plural = 'Students'
-	
-	def __str__(self):
-		return str(f'{self.user.first_name} {self.user.last_name} ({self.id})')
+    class Meta:
+        verbose_name = "Student"
+        verbose_name_plural = "Students"
 
-	def delete(self, *args, **kwargs):
-		# Delete the student
-		super().delete(*args, **kwargs)
+    def __str__(self):
+        return str(f"{self.user.first_name} {self.user.last_name} ({self.id})")
+
+    def delete(self, *args, **kwargs):
+        # Delete the student
+        super().delete(*args, **kwargs)
