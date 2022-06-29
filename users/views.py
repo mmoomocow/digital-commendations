b59113a0eabd425d6a12574d0983f94487b3227e
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-06-29 17:28:04 +1200

    Remove unused imports
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
