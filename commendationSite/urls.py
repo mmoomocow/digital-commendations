5ad06a82e142141f89026e2d048d3b5ff671b28d
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-07-18 16:58:10 +1200

    Add URL routing for teachers app
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- commendationSite/urls.py
+++ commendationSite/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path("users/", include("users.urls")),
     path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
+    path("teachers/", include("teachers.urls")),
     path("", include("home.urls")),
 ]
