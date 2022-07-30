06415b72490866c783ba84b3f9f09638dc9c4fc8
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-07-30 14:47:56 +1200

    Use MySQL in production
    Signed-off-by: mmoomocow <44288823+mmoomocow@users.noreply.github.com>

--- commendationSite/settings.py
+++ commendationSite/settings.py
@@ -85,12 +85,25 @@ WSGI_APPLICATION = "commendationSite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+# Use mysql if in production, otherwise use sqlite
+if os.environ.get("PRODUCTION", False):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_USER"),
+            "PASSWORD": os.environ.get("DB_PASSWORD"),
+            "HOST": os.environ.get("DB_HOST"),
+            "PORT": os.environ.get("DB_PORT"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
