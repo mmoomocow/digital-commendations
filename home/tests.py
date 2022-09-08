21fb817b44236c3fa2249524f2a61e5523d8f4c3
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-09-08 12:38:13 +1200

    Test that home page works

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from commendationSite import testHelper
 
 # Create your tests here.
+
+
+class TestHomePages(TestCase):
+    def test_home_page(self):
+        testHelper.testPage(self, "/", "home/index.html")
