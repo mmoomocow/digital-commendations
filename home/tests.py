a47e7be684ae08db39d6a429fe939d53f906884a
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-09-08 12:16:41 +1200

    Clear tests Starting from a clean state

--- home/tests.py
+++ home/tests.py
@@ -1,10 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-
-
-class testHomePage(TestCase):
-    def test_home_page(self):
-        response = self.client.get("/")
-        self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, "base.html")
