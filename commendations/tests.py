bb69d043b56c1acdce534e53b977cbc4e78e6fd0
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-09-09 09:23:57 +1200

    Test framework for commendations

--- commendations/tests.py
+++ commendations/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Commendation, Milestone
+from commendationSite import testHelper
 
 # Create your tests here.
+
+class commendationsTest(TestCase):
+    def setUp(self):
+        self.teacher = testHelper.createTeacher(self, is_management=False)
+        self.student = testHelper.createStudent(self)
+        self.client.login(username=self.teacher.username, password="password")
+
+    def tearDown(self):
+        pass
