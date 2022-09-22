48f0bf8b11cc5f9ff3f7b81681c5aa428545c21e
mmoomocow <44288823+mmoomocow@users.noreply.github.com>
2022-09-22 17:40:27 +1200

    Add contact model and view

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 
@@ -6,3 +7,17 @@ from django.shortcuts import render
 def index(request) -> render:
     """The home page of the website."""
     return render(request, "home/index.html")
+
+
+def contact(request) -> render:
+    """The contact page of the website."""
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+        contact = Contact(name=name, email=email, subject=subject, message=message)
+        contact.save()
+
+        return render(request, "home/contacted.html", status=201)
+    return render(request, "home/contact.html")
