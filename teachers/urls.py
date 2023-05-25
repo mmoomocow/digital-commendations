5fda34985b977781338afacc3f055ea7ebf67bd5
Matthew Gray <44288823+mmoomocow@users.noreply.github.com>
2023-05-26 09:28:18 +1200

    style: 🎨 intentionally bad styling to test workflows

--- teachers/urls.py
+++ teachers/urls.py
@@ -1,7 +1,8 @@
+from . import views
 from django.urls import path
 
-from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index,
+          name="index"),
 ]
