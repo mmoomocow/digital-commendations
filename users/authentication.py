7b29a0c454618b7095fbe0c0e95ee7874fcc354e
Matthew Gray <44288823+mmoomocow@users.noreply.github.com>
2023-06-01 12:36:28 +1200

    feat(backend): 🛂 Define helper code for authentication
    Creates authenticate method for views to authenticate with
    
    Signed-off-by: Matthew Gray <44288823+mmoomocow@users.noreply.github.com>

---
+++ users/authentication.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.contrib import auth
+from django.core.exceptions import ImproperlyConfigured, PermissionDenied
+from django.views.decorators.debug import sensitive_variables
+
+from users.ms_auth_backend import MS_auth_backend as ms_backend
+
+BACKEND_PATH = "users.ms_auth_backend.MS_auth_backend"
+
+if BACKEND_PATH not in settings.AUTHENTICATION_BACKENDS:
+    raise ImproperlyConfigured(f"{BACKEND_PATH} is not in AUTHENTICATION_BACKENDS")
+
+BACKEND = auth.load_backend(BACKEND_PATH)
+
+
+@sensitive_variables("creds")
+def authenticate(request, **creds):
+    try:
+        user = BACKEND.authenticate(request)
+    except PermissionDenied:
+        return None
+
+    if user is not None:
+        user.backend = BACKEND_PATH
+        return user
