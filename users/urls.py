43f7c93d7fd96245d69b5f59b7492225cb69a948
Matthew Gray <44288823+mmoomocow@users.noreply.github.com>
2023-06-07 21:08:50 +1200

    refactor(urls): ♻️ Better urls.py in users app
    Import views not import from views
    
    Signed-off-by: Matthew Gray <44288823+mmoomocow@users.noreply.github.com>

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
 
-from .views import loginView, logoutView
+from . import views
 
-urlpatterns = [
-    path("login/", loginView, name="login"),
-    path("logout/", logoutView, name="logout"),
-]
+urlpatterns = []
