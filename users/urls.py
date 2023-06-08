9dc186b0dc0d29bac6f24bc6ccf06ad00e0798fb
Matthew Gray <44288823+mmoomocow@users.noreply.github.com>
2023-06-08 12:26:43 +1200

    feat(urls): 🚚 Add url path for callback
    Signed-off-by: Matthew Gray <44288823+mmoomocow@users.noreply.github.com>

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path("callback/", views.callback, name="callback"),
 ]
