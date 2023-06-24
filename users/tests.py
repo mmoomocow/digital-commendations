ac887555d6f6086fa989343b7596621dabb99d91
Matthew Gray <44288823+mmoomocow@users.noreply.github.com>
2023-06-24 11:00:44 +0100

    test(auth): 🧪 Add microsoft auth testing
    Add testing for microsoft auth backend
    
    Signed-off-by: Matthew Gray <44288823+mmoomocow@users.noreply.github.com>

--- users/tests.py
+++ users/tests.py
@@ -1,10 +1,20 @@
-from django.test import TestCase
+from unittest.mock import patch
+
+from django.contrib.auth import get_backends, get_user_model
+from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.messages.storage.fallback import FallbackStorage
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.core.exceptions import ImproperlyConfigured
+from django.shortcuts import reverse
+from django.test import RequestFactory, TestCase
 
 from commendationSite import testHelper
 from students.models import Caregiver, Student
 from teachers.models import Teacher
 
+from .backends import TENANT_DOMAIN, MicrosoftAuthBackend
 from .models import User
+from .views import callback, login
 
 # Create your tests here.
 
@@ -52,6 +62,7 @@ class UserModelTest(TestCase):
 
 class UserViewsTest(TestCase):
     def setUp(self):
+        self.request_factory = RequestFactory()
         self.teacher = testHelper.createTeacher(self, is_management=True)
         self.student = testHelper.createStudent(self)
         self.user = testHelper.createUser(self)
@@ -101,3 +112,205 @@ class UserViewsTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, "/")
         self.assertEqual(response.wsgi_request.user.is_authenticated, False)
+
+    def test_callback_view_successful_authentication(self):
+        # Simulate a successful authentication callback
+        user = User.objects.create(email="johndoe@example.com")
+        request = self.request_factory.get("/", {"code": "dummy_code"})
+        request.user = user
+        request.session = {}
+        middleware = SessionMiddleware(get_response=lambda r: None)
+        middleware.process_request(request)
+        messages = FallbackStorage(request)
+        request._messages = messages
+
+        response = callback(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(request.user.is_authenticated)
+
+    def test_callback_view_invalid_authentication(self):
+        # Simulate an authentication callback with invalid user
+        request = self.request_factory.get("/", {"code": "dummy_code"})
+        request.user = AnonymousUser()
+        request.session = {}
+        middleware = SessionMiddleware(get_response=lambda r: None)
+        middleware.process_request(request)
+        messages = FallbackStorage(request)
+        request._messages = messages
+
+        response = callback(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(request.user.is_authenticated)
+
+    def test_no_backend(self):
+        # Remove the MicrosoftAuthBackend from the settings and test that login and callback return ImproperlyConfigured
+        with self.settings(AUTHENTICATION_BACKENDS=[]):
+            with self.assertRaises(ImproperlyConfigured):
+                self.client.get("/users/login/")
+            with self.assertRaises(ImproperlyConfigured):
+                self.client.get("/users/callback/")
+
+    def test_login_already_authenticated(self):
+        self.client.force_login(self.teacher)
+        response = self.client.get("/users/login/")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/")
+
+    def test_login_no_credentials(self):
+        response = self.client.post("/users/login/", {}, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/login.html")
+        self.assertContains(response, "Please enter both username and password.")
+        self.assertEqual(response.wsgi_request.user.is_authenticated, False)
+
+    def test_login_inactivated_user(self):
+        self.user.is_active = False
+        self.user.save()
+        response = self.client.post(
+            "/users/login/",
+            {"username": self.user.username, "password": "password"},
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/login.html")
+        self.assertEqual(response.wsgi_request.user.is_authenticated, False)
+
+    def test_logout_not_authenticated(self):
+        response = self.client.get("/users/logout/")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/")
+
+    def test_callback_no_code(self):
+        response = self.client.get("/users/callback/")
+        # Should redirect to login page
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "/login/")
+
+
+class MockResponse:
+    def __init__(self, json_data, status_code=200):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
+
+
+class MicrosoftAuthBackendTest(TestCase):
+    def setUp(self):
+        self.backend = MicrosoftAuthBackend()
+        self.request_factory = RequestFactory()
+
+    def test_setup(self):
+        request = self.request_factory.get("/")
+        request.session = {}
+        self.backend.setup(request)
+        self.assertIsNotNone(request.session.get(self.backend.SESSION_KEY))
+        self.assertIsNotNone(request.session[self.backend.SESSION_KEY].get("flow"))
+
+    def test_get_auth_uri(self):
+        request = self.request_factory.get("/")
+        request.session = {}
+        auth_uri = self.backend.get_auth_uri(request)
+        self.assertTrue(auth_uri.startswith("https://login.microsoftonline.com"))
+
+    def test_authenticate_successful(self):
+        # Simulate a successful authentication request
+        user_data = {
+            "givenName": "John",
+            "surname": "Doe",
+            "mail": f"johndoe@{TENANT_DOMAIN}",
+        }
+        access_token = "dummy_access_token"
+        request = self.request_factory.get("/")
+        request.GET = {"code": "dummy_code"}
+        request.session = {
+            self.backend.SESSION_KEY: {"flow": {"dummy_key": "dummy_value"}}
+        }  # Update the value to a dictionary
+
+        # Mock the requests.get method to return the user data
+        def mock_get(url, headers):
+            return MockResponse(user_data)
+
+        # Patch the ms_client.acquire_token_by_auth_code_flow method
+        with patch.object(
+            self.backend.ms_client, "acquire_token_by_auth_code_flow"
+        ) as mock_acquire_token:
+            mock_acquire_token.return_value = {"access_token": access_token}
+            with patch("requests.get", side_effect=mock_get):
+                user = self.backend.authenticate(request)
+
+        # Assert the user and user data
+        self.assertIsNotNone(user)
+        self.assertEqual(user.username, "johndoe")
+        self.assertEqual(user.email, f"johndoe@{TENANT_DOMAIN}")
+        self.assertEqual(user.first_name, "John")
+        self.assertEqual(user.last_name, "Doe")
+
+    def test_authenticate_no_code(self):
+        # Simulate an authentication request without a code
+        request = self.request_factory.get("/")
+        request.GET = {}
+        request.session = {
+            self.backend.SESSION_KEY: {"flow": {"dummy_key": "dummy_value"}}
+        }
+
+        user = self.backend.authenticate(request)
+
+        # Assert that no user is returned
+        self.assertIsNone(user)
+
+    def test_authenticate_no_flow(self):
+        # Simulate an authentication request without a flow
+        request = self.request_factory.get("/")
+        request.GET = {"code": "dummy_code"}
+        request.session = {}
+
+        user = self.backend.authenticate(request)
+
+        # Assert that no user is returned
+        self.assertIsNone(user)
+
+    def test_authenticate_invalid_token(self):
+        # Simulate an authentication request with an invalid token
+        request = self.request_factory.get("/")
+        request.GET = {"code": "dummy_code"}
+        request.session = {self.backend.SESSION_KEY: {"flow": "dummy_flow"}}
+
+        # Mock the ms_client.acquire_token_by_auth_code_flow method to raise a ValueError
+        with patch.object(
+            self.backend.ms_client,
+            "acquire_token_by_auth_code_flow",
+            side_effect=ValueError,
+        ):
+            user = self.backend.authenticate(request)
+            self.assertIsNone(user)
+
+    def test_authenticate_invalid_email(self):
+        # Simulate a successful authentication request
+        user_data = {
+            "givenName": "John",
+            "surname": "Doe",
+            "mail": f"johndoe@example.com",
+        }
+        access_token = "dummy_access_token"
+        request = self.request_factory.get("/")
+        request.GET = {"code": "dummy_code"}
+        request.session = {
+            self.backend.SESSION_KEY: {"flow": {"dummy_key": "dummy_value"}}
+        }  # Update the value to a dictionary
+
+        # Mock the requests.get method to return the user data
+        def mock_get(url, headers):
+            return MockResponse(user_data)
+
+        # Patch the ms_client.acquire_token_by_auth_code_flow method
+        with patch.object(
+            self.backend.ms_client, "acquire_token_by_auth_code_flow"
+        ) as mock_acquire_token:
+            mock_acquire_token.return_value = {"access_token": access_token}
+            with patch("requests.get", side_effect=mock_get):
+                user = self.backend.authenticate(request)
+
+        # Assert the user and user data
+        self.assertIsNone(user)
