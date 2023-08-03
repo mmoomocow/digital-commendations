52efdea8a3df127043ade71d4d6c84fddf927b95
Matthew Gray <44288823+mmoomocow@users.noreply.github.com>
2023-08-04 09:10:22 +1200

    ci(signals): 💚 skip PYL-W0613 on signal function
    Signed-off-by: Matthew Gray <44288823+mmoomocow@users.noreply.github.com>

--- users/signals.py
+++ users/signals.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from .models import User
 
 
-def update_last_visit(_sender, **kwargs):
+def update_last_visit(sender, **kwargs):  # skipcq: PYL-W0613
     """Update the last viewed time."""
     user: User = kwargs["user"]
     user.previous_login = user.recent_login
