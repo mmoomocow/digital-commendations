320fef7f9c228bdab1f374ef26c793b15ec7acba
Matthew Gray <44288823+mmoomocow@users.noreply.github.com>
2023-08-16 18:10:05 +1200

    feat(forms): 🎨 Make student selector 2 column
    Creates a 2 column layout for the student selector. When a student is chosen they will be moved to the right column
    
    Signed-off-by: Matthew Gray <44288823+mmoomocow@users.noreply.github.com>

--- commendations/static/commendations/search.js
+++ commendations/static/commendations/search.js
@@ -1,10 +1,37 @@
 function filterStudents(searchTerm) {
-    var students = document.getElementsByClassName("studentChoice");
-    for (var i = 0; i < students.length; i++) {
-        if (students[i].text.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
-            students[i].style.display = "";
+    var selectableStudents = document.getElementById('selectable-students').getElementsByTagName("li");
+    var selectedStudents = document.getElementById('selected-students').getElementsByTagName("li");
+
+    if (searchTerm == "") {
+        for (var i = 0; i < selectableStudents.length; i++) {
+            selectableStudents[i].style.display = "";
+        }
+
+        for (var i = 0; i < selectedStudents.length; i++) {
+            selectedStudents[i].style.display = "";
+        }
+
+        return;
+    }
+
+
+    searchTerm = searchTerm.toLowerCase();
+
+    for (var i = 0; i < selectableStudents.length; i++) {
+        var studentName = selectableStudents[i].innerHTML.toLowerCase();
+        if (studentName.indexOf(searchTerm) > -1) {
+            selectableStudents[i].style.display = "";
         } else {
-            students[i].style.display = "none";
+            selectableStudents[i].style.display = "none";
+        }
+    }
+
+    for (var i = 0; i < selectedStudents.length; i++) {
+        var studentName = selectedStudents[i].innerHTML.toLowerCase();
+        if (studentName.indexOf(searchTerm) > -1) {
+            selectedStudents[i].style.display = "";
+        } else {
+            selectedStudents[i].style.display = "none";
         }
     }
 }
